a377f98c18100d77f3ca06081caae8c7cf2b7cfe
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2021-11-21 14:59:18 -0300

    <new> Added username in Profile page

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid class="pa-0">
     <v-img
-      src="https://cdn.pixabay.com/photo/2018/02/01/14/09/yellow-3123271_960_720.jpg"
+      :src="require('@/assets/images/profile-mock/background.jpg')"
       alt="Profile background photo"
       width="100%"
       height="243px"
@@ -9,12 +9,14 @@
 
     <v-row cols="12">
       <v-col cols="4" class="d-flex flex-column align-center justify-center">
-        <v-avatar width="165px" height="165px" class="profile-avatar-photo">
+        <v-avatar width="165px" height="165px" class="profile-avatar-photo translated">
           <v-img
-            src="https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445_960_720.jpg"
+            :src="require('@/assets/images/profile-mock/man.jpg')"
             alt="Avatar photo"
           />
         </v-avatar>
+
+        <p class="master-title translated mt-3">@fernandosilva</p>
       </v-col>
     </v-row>
   </v-container>
@@ -22,7 +24,10 @@
 
 <style lang="scss" scoped>
 .profile-avatar-photo {
-  transform: translateY(-82.5px);
   border: 2px solid #FFF;
+}
+
+.translated {
+  transform: translateY(-82.5px); 
 }
 </style>
