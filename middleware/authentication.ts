5d16e1361ed126b93d37c80f68bab72363dd8971
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2021-11-27 17:58:34 -0300

    <modified> Added authentication verification for index route

--- middleware/authentication.ts
+++ middleware/authentication.ts
@@ -3,9 +3,13 @@ import { Middleware } from '@nuxt/types';
 const authentication: Middleware = ({ store, redirect, route }) => {
   const authenticated = store.getters.isAuthenticated;
   const loginOrSignUp = route.name === 'signIn' || route.name === 'signUp'
-  
+
   if (loginOrSignUp && authenticated) {
-    return redirect('/home')
+    return redirect('/home');
+  }
+
+  if (authenticated && route.name === 'index') {
+    return redirect('/home');
   }
 
   if (!authenticated && !loginOrSignUp) {
