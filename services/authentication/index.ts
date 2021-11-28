a680bc4f86a7f1c65c828c1fdb4a80980f97ebb3
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2021-11-28 18:07:36 -0300

    <modified> Created set methods | <modified> Improved auth mechanism

--- services/authentication/index.ts
+++ services/authentication/index.ts
@@ -1,17 +1,48 @@
 import { auth } from '@/plugins/firebase';
-import { GoogleAuthProvider, signInWithRedirect, signOut } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  signInWithRedirect,
+  signOut,
+  getRedirectResult,
+  Auth
+} from 'firebase/auth';
+import Database, { IDatabase }  from '../database';
 
 const googleAuthProvider = new GoogleAuthProvider();
 
 export interface IAuthentication {
+  database: IDatabase;
+  auth: Auth;
   signInWithGoogle: () => void;
   signOut: () => Promise<boolean>;
 };
 
 class Authentication implements IAuthentication {
+  database;
+  auth;
+
+  constructor () {
+    this.auth = auth;
+    this.database = new Database('users');
+  };
+
   signInWithGoogle () {
-    signInWithRedirect(auth, googleAuthProvider)
+    signInWithRedirect(auth, googleAuthProvider);
   }
+
+  async checkGoogleAuthResults () {
+    const results = await getRedirectResult(auth);
+
+    if (!!results) {
+      console.log('FUIII CHAAMMAAADOOO')
+      
+      await this.database.setIfNotExists({
+        name: results.user.displayName,
+      }, 'teste')
+    };
+
+    console.log('REDIRECT_RESULT', results);
+  };
 
   async signOut () {
     try {
--- services/database/index.ts
+++ services/database/index.ts
@@ -4,6 +4,7 @@ import {
   DatabaseReference,
   ref as DatabaseRef,
   get as DatabaseGet,
+  set as DatabaseSet,
   child as DatabaseChild,
   DataSnapshot
 } from 'firebase/database';
@@ -12,6 +13,7 @@ export interface IDatabase {
   database: FirebaseDatabase;
   reference: DatabaseReference;
   get: (childPath?: string) => Promise<DataSnapshot | null>;
+  set: (content: any, childPath?: string) => Promise<boolean>;
 };
 
 class Database implements IDatabase {
@@ -39,9 +41,51 @@ class Database implements IDatabase {
 
       return null;
     } catch (err) {
-      console.log('Error on database service', err);
+      console.log('Error on database service (GET)', err);
       return null;
     };
+  };
+
+  async set (content: any, childPath?: string) {
+    try {
+      if (!!childPath) {
+        await DatabaseSet(DatabaseChild(this.reference, childPath), content);
+      } else {
+        await DatabaseSet(this.reference, content);
+      };
+
+      return true;
+    } catch (err) {
+      console.log('Error on database service (SET)', err);
+      return false;
+    }
+  };
+
+  async setIfNotExists (content: any, childPath?: string) {
+    try {
+      if (!!childPath) {
+        const alreadyExists = await this.get(childPath);
+
+        if (alreadyExists) {
+          throw new Error('Data already exists');
+        }
+
+        await DatabaseSet(DatabaseChild(this.reference, childPath), content);
+      } else {
+        const alreadyExists = await this.get();
+
+        if (alreadyExists) {
+          throw new Error('Data already exists');
+        }
+
+        await DatabaseSet(this.reference, content);
+      };
+
+      return true;
+    } catch (err) {
+      console.log('Error on database service (SET)', err);
+      return false;
+    }
   };
 };
 
