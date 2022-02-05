e8005ceeaa6e5770a9d8f55aea4e256cf2aba8c2
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-02-04 23:39:22 -0300

    <new> Added toggleSave method to PostActions component

--- src/components/pages/posts/PostActions.vue
+++ src/components/pages/posts/PostActions.vue
@@ -15,18 +15,20 @@
     />
 
     <IconButtonTooltip
-      icon="mdi-bookmark-outline"
+      :icon="postSaved ? 'mdi-bookmark' : 'mdi-bookmark-outline'"
       text="Save"
       size="30"
-      :action="() => {}"
+      :action="toggleSave"
     />
   </v-col>
 </template>
 
 <script lang="ts">
 import { Mixins, Component, Prop } from 'vue-property-decorator';
+import { Action } from 'vuex-class';
 
 import PostsService from '@/services/posts';
+import Users, { IUsers } from '@/services/users';
 
 import IconButtonTooltip from '@/components/commons/IconButtonTooltip.vue';
 
@@ -39,10 +41,21 @@ import { TPost } from '@/types/posts';
 })
 export default class PostActionsComponent extends Mixins(OnFirestoreUserData) {
   postsService = new PostsService();
+  usersService: IUsers | null = null;
 
   @Prop(Object) readonly post!: TPost;
   @Prop(Function) readonly updatePage!: () => Promise<void>;
   @Prop(Function) readonly toggleTextarea!: () => void;
+
+  @Action getCurrentFirestoreUser!: () => Promise<void>;
+
+  created () {
+    const userId = this.firestoreUserId;
+
+    if (userId) {
+      this.usersService = new Users(userId);
+    };
+  };
 
   get postLiked () {
     if (!this.post.likes) return false;
@@ -50,10 +63,23 @@ export default class PostActionsComponent extends Mixins(OnFirestoreUserData) {
     return this.post.likes.findIndex(like => like.author_id === this.firestoreUserId) !== -1;
   };
 
+  get postSaved () {
+    return this.firestoreUser?.savedPosts && this.firestoreUser.savedPosts.indexOf(this.post.id) !== -1;
+  };
+
   async toggleLike () {
     const success = await this.postsService.toggleLike(this.post.id, this.firestoreUserId);
 
     if (success) await this.updatePage();
+  };
+
+  async toggleSave () {
+    const success = await this.usersService?.toggleSavedPost(this.post.id);
+
+    if (success) {
+      await this.getCurrentFirestoreUser();
+      await this.updatePage();
+    };
   };
 };
 </script>
