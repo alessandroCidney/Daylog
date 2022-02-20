06f92658dacea0a06dc23395070b3f9cf5168c92
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-02-20 20:51:42 -0300

    <modified> Removed unnecessary data in settings page code

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -13,157 +13,17 @@
       <v-tab-item class="base"><AccountTab /></v-tab-item>
       <v-tab-item class="base"><DataTab /></v-tab-item>
     </v-tabs>
-
-    <v-row align="center" justify="center" class="my-10" v-if="false">
-      <v-col md="8" sm="10" cols="10" class="light rounded-xl pa-0">
-        <v-row align="center" justify="center" class="photosArea">
-          <v-col cols="12">
-            <v-img
-              width="100%"
-              :height="backgroundPhotoLoaded ? '200px' : 0"
-              class="overflow-hidden rounded-t-xl"
-              :src="firestoreUserProfileBackground || require('@/assets/images/b-background.jpg')"
-              @load="backgroundPhotoLoaded = true"
-            />
-
-            <v-skeleton-loader
-              v-if="!backgroundPhotoLoaded"
-              type="image"
-              width="100%"
-              height="200px"
-            />
-          </v-col>
-
-          <v-col cols="6" class="d-flex align-center justify-center">
-            <v-avatar
-              :width="profilePhotoLoaded ? '150px' : 0"
-              :height="profilePhotoLoaded ? '150px' : 0"
-              :class="{
-                'avatarPhoto': true ,
-                'zeroWidth': !profilePhotoLoaded
-              }"
-            >
-              <v-img
-                :src="firestoreUserProfilePhoto || require('@/assets/images/profile/user.jpg')"
-                alt="Avatar photo"
-                :width="profilePhotoLoaded ? '150px' : 0"
-                :height="profilePhotoLoaded ? '150px' : 0"
-                @load="profilePhotoLoaded = true"
-              />
-            </v-avatar>
-
-            <v-skeleton-loader
-              v-if="!profilePhotoLoaded"
-              class="profilePhotoLoader"
-              type="image"
-              width="150px"
-              height="150px"
-            />
-          </v-col>
-        </v-row>
-
-        <v-row class="pa-4">
-          <v-col cols="6" class="d-flex align-center justify-start">
-            <v-switch
-              v-model="lightTheme"
-              color="yellow"
-              prepend-icon="mdi-brightness-4"
-            />
-          </v-col>
-
-          <v-col cols="6" class="d-flex align-center justify-end">
-            <v-btn
-              text
-              @click="allowEdit = true"
-            >
-              <v-icon color="space" left>mdi-pencil</v-icon>
-              Edit
-            </v-btn>
-          </v-col>
-        </v-row>
-
-        <v-row align="center" justify="center" class="pa-4">
-          <v-col cols="12">
-            <v-text-field
-              v-model="changes.username"
-              label="Username"
-              :value="firestoreUserUsername"
-              prefix="@"
-              :disabled="!allowEdit"
-              :color="lightTheme ? 'space' : 'white'"
-            />
-          </v-col>
-
-          <v-col cols="6">
-            <v-file-input
-              v-model="changes.profilePhoto"
-              prepend-icon="mdi-camera-plus"
-              placeholder="Change profile photo"
-              :disabled="!allowEdit"
-              :color="lightTheme ? 'space' : 'white'"
-            />
-          </v-col>
-
-          <v-col cols="6">
-            <v-file-input
-              v-model="changes.profileBackgroundPhoto"
-              prepend-icon="mdi-tooltip-image"
-              placeholder="Change profile background photo"
-              :disabled="!allowEdit"
-              :color="lightTheme ? 'space' : 'white'"
-            />
-          </v-col>
-        </v-row>
-
-        <v-row align="center" justify="center" class="pa-4">
-          <v-col cols="12">
-            <v-btn
-              color="space"
-              class="white--text"
-              width="100%"
-              :loading="saveChangesLoading"
-              @click="handleSaveChanges"
-            >
-              Save changes
-            </v-btn>
-          </v-col>
-          
-          <v-col cols="12">
-            <v-btn
-              color="error"
-              class="white--text"
-              width="100%"
-              :loading="deleteAccountLoading"
-              @click="handleDeleteAccount"
-            >
-              Delete my account
-            </v-btn>
-          </v-col>
-
-          <v-col cols="12">
-            <v-btn
-              plain
-              width="100%"
-              @click="handleDownloadAccountData"
-            >
-              Download account data
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-col>  
-    </v-row>
   </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Mixins, Watch } from 'vue-property-decorator';
+import { Component, Mixins } from 'vue-property-decorator';
 
 import AccountTab from '@/components/pages/settings/tabs/AccountTab.vue';
 import ProfileTab from '@/components/pages/settings/tabs/ProfileTab.vue';
 import AppearanceTab from '@/components/pages/settings/tabs/AppearanceTab.vue';
 import DataTab from '@/components/pages/settings/tabs/DataTab.vue';
 
-import Authentication from '@/services/authentication';
 import OnSetUserData from '@/mixins/OnSetUserData';
 
 import _ from 'lodash';
@@ -171,53 +31,7 @@ import _ from 'lodash';
 @Component({
   components: { AccountTab, ProfileTab, AppearanceTab, DataTab }
 })
-export default class SettingsPage extends Mixins(OnSetUserData) {
-  authenticationService = new Authentication();
-  allowEdit = false;
-  lightTheme = !this.$vuetify.theme.dark;
-  backgroundPhotoLoaded = false;
-  profilePhotoLoaded = false;
-  deleteAccountLoading = false;
-
-  created () {
-    this.changes.username = this.firestoreUserUsername;
-  };
-
-  @Watch('lightTheme')
-  async onLightThemeChanged (activeLightTheme: boolean) {
-    this.$vuetify.theme.dark = !activeLightTheme;
-    await this.usersService?.changeTheme(!activeLightTheme);
-    await this.getCurrentFirestoreUser();
-  };
-
-  async handleDeleteAccount () {
-    this.deleteAccountLoading = true;
-
-    if (!this.firestoreUserId || !this.firestoreUserEmail) return;
-
-    await this.authenticationService.deleteAccount(
-      this.firestoreUserId,
-      this.firestoreUserEmail
-    );
-
-    this.deleteAccountLoading = false;
-    this.$router.push('/');
-  };
-
-  async handleDownloadAccountData () {
-    const fileDataURL = await this.usersService?.getAccountData() || '';
-    
-    const anchor = document.createElement('a');
-    anchor.classList.add('dont-show');
-    anchor.setAttribute('href', fileDataURL);
-    anchor.setAttribute('download', 'daylog.zip');
-
-    document.body.appendChild(anchor);
-    anchor.click();
-
-    document.body.removeChild(anchor);
-  };
-};
+export default class SettingsPage extends Mixins(OnSetUserData) {};
 </script>
 
 <style lang="scss">
