d8cdfc97b187e4d4fa2f17c82f6e4b6dfbad2c6a
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-02-20 12:16:21 -0300

    <modified> Adapted PopoverMenu component to use vue-property-decorator

--- src/components/layouts/default/AppBar/components/PopoverMenu.vue
+++ src/components/layouts/default/AppBar/components/PopoverMenu.vue
@@ -12,7 +12,7 @@
         :class="!avatarLoaded && 'invisible-avatar'"
       >
         <v-img
-          :src="profilePhoto || require('@/assets/images/profile/user.jpg')"
+          :src="firestoreUserProfilePhoto || require('@/assets/images/profile/user.jpg')"
           alt="Profile photo"
           :width="avatarLoaded ? '46px' : '0'"
           @load="avatarLoaded = true"
@@ -31,14 +31,14 @@
         <v-list-item>
           <v-list-item-avatar>
             <v-img
-              :src="profilePhoto || require('@/assets/images/profile/user.jpg')"
+              :src="firestoreUserProfilePhoto || require('@/assets/images/profile/user.jpg')"
               alt="Profile photo"
             />
           </v-list-item-avatar>
 
           <v-list-item-content>
-            <v-list-item-title class="overflow-200">@{{ username }}</v-list-item-title>
-            <v-list-item-subtitle class="overflow-200">Iniciante</v-list-item-subtitle>
+            <v-list-item-title class="overflow-200">@{{ firestoreUserUsername }}</v-list-item-title>
+            <v-list-item-subtitle class="overflow-200">Beginner</v-list-item-subtitle>
           </v-list-item-content>
 
           <v-list-item-action>
@@ -64,7 +64,7 @@
             </v-list-item-title>
           </v-list-item>
 
-          <v-list-item :to="`/users/${userId}`" @click="showMenu = false">
+          <v-list-item to="/users/me" @click="showMenu = false">
             <v-list-item-icon>
               <v-icon color="space">mdi-account-circle</v-icon>
             </v-list-item-icon>
@@ -90,80 +90,26 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import { mapState } from 'vuex';
-import { StoreUser, FirestoreUser } from '@/types/users';
+import { Component, Mixins } from 'vue-property-decorator';
+
+import { OnFirestoreUserData } from '@/mixins';
 
 import Authentication from '@/services/authentication';
-import Database, { IDatabase } from '@/services/database';
-import { IAuthentication } from '@/types/services/authentication';
+import Database from '@/services/database';
 
-interface Data {
-  userId: string;
-  showMenu: boolean;
-  authenticationService: IAuthentication | null;
-  usersDatabase: IDatabase | null;
-  avatarLoaded: boolean;
-};
+@Component
+export default class PopoverMenuComponent extends Mixins(OnFirestoreUserData) {
+  showMenu = false;
+  authenticationService = new Authentication();
+  usersDatabase = new Database('users');
+  avatarLoaded = false;
 
-interface Methods {
-  handleSignOut: () => Promise<void>;
-};
-
-interface Computed {
-  user: StoreUser | null;
-  username: string;
-  profilePhoto: string;
-};
-
-interface Props {};
-
-export default Vue.extend<Data, Methods, Computed, Props>({
-  data: () => ({
-    userId: '',
-    showMenu: false,
-    authenticationService: null,
-    usersDatabase: null,
-    avatarLoaded: false,
-  }),
-
-  async created () {
-    this.authenticationService = new Authentication();
-    this.usersDatabase = new Database('users');
-
-    const user = await this.usersDatabase.getWhere(
-      'email',
-      this.user?.firestoreUser?.email
-    );
-
-    if (!user) {
-      return;
-    };
-
-    const [ userId ] = Object.keys(user);
-    this.userId = userId;
-  },
-
-  computed: {
-    ...mapState(['user']),
-
-    username () {
-      return this.user?.firestoreUser?.username || '';
-    },
-
-    profilePhoto () {
-      return this.user?.firestoreUser?.profile_photo || '';
-    }
-  },
-
-  methods: {
-    async handleSignOut () {
-      this.showMenu = false;
-      await this.authenticationService?.signOut();
-      this.$router.push('/signIn');
-    }
+  async handleSignOut () {
+    this.showMenu = false;
+    await this.authenticationService.signOut();
+    this.$router.push('/signIn');
   }
-});
+};
 </script>
 
 <style lang="scss">
