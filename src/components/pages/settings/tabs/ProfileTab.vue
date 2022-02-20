2b34d48d550b0f7316ced4b31b0343d0aa0bc931
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-02-20 13:18:46 -0300

    <new> Added edit profile data function

--- src/components/pages/settings/tabs/ProfileTab.vue
+++ src/components/pages/settings/tabs/ProfileTab.vue
@@ -5,8 +5,9 @@
     <h3 class="mb-5">Username</h3>
 
     <v-text-field
-      label="Username"
+      @input="changes.username = $event"
       :value="firestoreUserUsername"
+      label="Username"
       color="space"
       prefix="@"
     />
@@ -26,12 +27,14 @@
 
       <v-col cols="7">
         <v-file-input
+          v-model="changes.profilePhoto"
           prepend-icon="mdi-camera-plus"
           placeholder="Change profile photo"
           :color="!theme.isDark ? 'space' : 'white'"
         />
 
         <v-file-input
+          v-model="changes.profileBackgroundPhoto"
           prepend-icon="mdi-tooltip-image"
           placeholder="Change background picture"
           :color="!theme.isDark ? 'space' : 'white'"
@@ -41,7 +44,9 @@
 
     <div class="d-flex">
       <v-spacer />
-      <v-btn color="space" class="light--text" depressed>Save changes</v-btn>
+      <v-btn color="space" class="light--text mb-10" block depressed @click="handleSaveChanges">
+        Save changes
+      </v-btn>
     </div>
   </div>
 </template>
@@ -49,12 +54,12 @@
 <script lang="ts">
 import { Mixins, Component, Inject } from 'vue-property-decorator';
 
-import { OnFirestoreUserData } from '@/mixins';
+import { OnSetUserData } from '@/mixins';
 
 type TInjectedTheme = { isDark: boolean; };
 
 @Component
-export default class AccountTabComponent extends Mixins(OnFirestoreUserData) {
+export default class AccountTabComponent extends Mixins(OnSetUserData) {
   @Inject({ default: { isDark: false } }) readonly theme!: TInjectedTheme;
 };
 </script>
