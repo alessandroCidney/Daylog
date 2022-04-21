e201fb6e4de2f3a569dfa24765a7e455dd494a30
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-04-21 15:25:35 -0300

    feat: Updated privacy policy

--- src/pages/privacy.vue
+++ src/pages/privacy.vue
@@ -1,5 +1,13 @@
 <template>
-  
+  <v-container class="pt-10">
+    <h1 class="privacy-title">Privacy Policy</h1>
+
+    <h2>Introduction</h2>
+
+    <p>
+      Welcome to Logbook privacy policy. This documents contains informations about how we collect, use and share your data. 
+    </p>
+  </v-container>
 </template>
 
 <script>
@@ -10,3 +18,10 @@ import { Vue, Component } from 'vue-property-decorator';
 })
 export default class PrivacyPage {};
 </script>
+
+<style lang="scss" scoped>
+.privacy-title {
+  font-size: 5em !important;
+  font-weight: 900 !important;
+}
+</style>
