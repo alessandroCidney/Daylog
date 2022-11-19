093219754145b8a1673dd7732f945baf234f038b
alessandroCidney <contatoalessandrocidneysa@gmail.com>
2022-11-19 15:47:11 -0300

    feat: Configured unit tests

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^~/(.*)$': '<rootDir>/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: [
+    'js',
+    'vue',
+    'json',
+    'ts',
+  ],
+  transform: {
+    '^.+\\.(js|ts)$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ],
+  testEnvironment: 'jsdom',
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)']
+}
